1c8b93d677083c744f347bcb22c776e0ee026842
Donghui-Lee <ehdgml3206@naver.com>
2020-03-01 23:25:35 +0900

    기초 작업 설정

---
+++ src/components/Counter.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+// 프레젠테이셔널 컴포넌트
+const Counter = ({ onIncrease, onDecrease, number }) => {
+  return (
+    <div>
+      <h1>{number}</h1>
+      <button onClick={onIncrease}>+1</button>
+      <button onClick={onDecrease}>-1</button>
+    </div>
+  );
+};
+
+export default Counter;
